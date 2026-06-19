5cade46b6ed336aecf2bcf35350a016d705846fc
wyltw <wyltw812@gmail.com>
2026-06-19 22:15:08 +0800

    chore: remove unused timer tabs import

--- src/components/timer/timer-tabs.tsx
+++ src/components/timer/timer-tabs.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "../ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Timer from "./timer";
 import type { TimerType } from "@/lib/types/types";
