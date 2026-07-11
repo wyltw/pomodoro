53feb4018d6d5863700a4a5b6eaef2921cfefb0a
wyltw <wyltw812@gmail.com>
2026-07-11 15:41:10 +0800

    feat(prisma): add client and seed setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,8 +7,10 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: `tsx prisma/seed.ts`,
   },
   datasource: {
     url: process.env["DATABASE_URL"],
+    shadowDatabaseUrl: process.env["SHADOW_DATABASE_URL"],
   },
 });
