f6f680148bd25a27b92207fa9cd1ec7456afdcbf
wyltw <wyltw812@gmail.com>
2026-07-24 15:00:13 +0800

    feat(header): link brand to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { AuthMenu } from "@/components/auth/auth-menu";
 import { SettingsDialog } from "@/components/settings/settings-dialog";
 import { StatisticsButton } from "@/components/statistics/statistics-button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import Link from "next/link";
 
 type HeaderProps = {
   showSidebarTrigger?: boolean;
@@ -11,7 +12,9 @@ export default function Header({ showSidebarTrigger = false }: HeaderProps) {
   return (
     <header className="flex h-12 shrink-0 items-center px-4">
       {showSidebarTrigger && <SidebarTrigger className="mr-2 md:hidden" />}
-      <h1 className="font-heading font-semibold">POMODORO</h1>
+      <Link href={"/"} className="cursor-pointer">
+        <h1 className="font-heading font-semibold">POMODORO</h1>
+      </Link>
       <ul className="ms-auto flex gap-2">
         <li>
           <SettingsDialog />
